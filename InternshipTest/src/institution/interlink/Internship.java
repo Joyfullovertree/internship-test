152997632f78dd09668548fce6f14af9ebabb73a
Joyfullovertree <sashahlopin1@gmail.com>
2018-08-04 23:53:40 +0300

    Add selection of interns

--- InternshipTest/src/institution/interlink/Internship.java
+++ InternshipTest/src/institution/interlink/Internship.java
@@ -1,14 +1,28 @@
 package institution.interlink;
 
 import person.Student;
+import java.util.ArrayList;
 
 public class Internship {
+    public String name;
+    public ArrayList<Student> interns = new ArrayList<>();
+
     public Internship(String name) {
         //TODO: Implementation is needed
+        this.name = name;
     }
 
     public void setStudent(Student student) {
         //TODO: Implementation is needed
+        interns.add(student);
+    }
+
+    public void selectStudents(ArrayList<Student> students) {
+        for (int i = 0; i<students.size(); i++) {
+            if (students.get(i).knowledge.level > 50) {
+                setStudent(students.get(i));
+            }
+        }
     }
 
     public String getStudents() {
