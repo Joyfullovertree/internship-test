8b63feed2994534fe82d7eb1241b27f2dac9ab2f
Joyfullovertree <sashahlopin1@gmail.com>
2018-08-04 23:21:28 +0300

    Realize constructors of Student class

--- InternshipTest/src/person/Student.java
+++ InternshipTest/src/person/Student.java
@@ -3,8 +3,26 @@ package person;
 import person.consciousness.Knowledge;
 
 public class Student {
+    public String firstName = "";
+    public String lastName = "";
+    public String name = "";
+    
     public Student(String name) {
         //TODO: Implementation is needed
+        this.name = name;
+        //Separation of name for firstName and lastName
+        if (this.name.indexOf(" ") != -1) {
+            String[] names = this.name.split(" ");
+            firstName = names[0];
+            lastName = names[1];
+        }
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        //Unification of firstName and lastName into name
+        name = String.join(" ", this.firstName, this.lastName);
     }
 
     public void setKnowledge(Knowledge knowledge) {
